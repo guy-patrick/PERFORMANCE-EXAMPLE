1385695c3f0e9a7e2001855b49898e6639febf44
Patrick OUAMBO <patgp04@gmail.com>
2022-09-14 14:04:21 +0100

    pm2 configs

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ function delay(durarion) {
 }
 
 app.get("/", (req, res) => {
-  res.send("Performance example");
+  res.send(`Performance example - ${process.pid}`);
 });
 
 app.get("/timer", (req, res) => {
   delay(9000);
-  res.send("Ding ding ding!");
+  res.send(`Ding ding ding! - ${process.pid}`);
 });
 
+console.log("Worker process started");
 app.listen(3000, () => {
   console.log("listening ...");
 });
